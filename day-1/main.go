fdf6f18e699c91400371b83ef44aeefe8e980785
Nicolas Lopes <nicolaslopesaquino@gmail.com>
2023-12-01 13:45:46 -0300

    part-1

--- day-1/main.go
+++ day-1/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"strconv"
+	"strings"
 )
 
 func Run() {
@@ -13,5 +15,33 @@ func Run() {
 		log.Fatalf("Error reading file: %v", err)
 	}
 
-	fmt.Println(string(body))
+	lines := strings.Split(string(body), "\n")
+
+	sum := 0
+
+	for _, line := range lines {
+		first := "0"
+		last := "0"
+
+		for i, c := range line {
+			_, err := strconv.ParseInt(string(c), 10, 64)
+
+			if err == nil {
+				if i == 0 || first == "0" {
+					first = string(c)
+				}
+
+				last = string(c)
+			}
+		}
+		calibrationValue, err := strconv.Atoi(first + last)
+
+		if err != nil {
+			log.Fatalf("Error converting string to int: %v", err)
+		}
+
+		sum += calibrationValue
+	}
+
+	fmt.Println("Result: %v", sum)
 }
