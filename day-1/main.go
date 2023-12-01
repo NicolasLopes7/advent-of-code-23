678dcafdc41ed54a7a45f189aabf443702248480
Nicolas Lopes <nicolaslopesaquino@gmail.com>
2023-12-01 17:28:18 -0300

    part 2

--- day-1/main.go
+++ day-1/main.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"regexp"
 	"strconv"
 	"strings"
+	"unicode"
 )
 
 func Run() {
@@ -17,6 +19,11 @@ func Run() {
 
 	lines := strings.Split(string(body), "\n")
 
+	// part1(lines)
+	part2(lines)
+}
+
+func part1(lines []string) {
 	sum := 0
 
 	for _, line := range lines {
@@ -24,9 +31,9 @@ func Run() {
 		last := "0"
 
 		for i, c := range line {
-			_, err := strconv.ParseInt(string(c), 10, 64)
+			isDigit := unicode.IsDigit(c)
 
-			if err == nil {
+			if isDigit {
 				if i == 0 || first == "0" {
 					first = string(c)
 				}
@@ -43,5 +50,70 @@ func Run() {
 		sum += calibrationValue
 	}
 
-	fmt.Println("Result: %v", sum)
+	fmt.Println("Result:", sum)
+}
+
+func part2(lines []string) {
+	parsedReverse := reverseSlice(lines)
+
+	stringDigits := []string{
+		"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+	}
+	stringDigitsReverse := reverseSlice(stringDigits)
+
+	firstNumREQuery := `\d`
+	for _, digit := range stringDigits {
+		firstNumREQuery += `|` + digit
+	}
+
+	lastNumREQuery := `\d`
+	for _, digit := range stringDigitsReverse {
+		lastNumREQuery += `|` + digit
+	}
+
+	firstNumRE := regexp.MustCompile(firstNumREQuery)
+	lastNumRE := regexp.MustCompile(lastNumREQuery)
+
+	var sum = 0
+
+	for i, line := range lines {
+		lineReverse := parsedReverse[i]
+
+		first := digitToStr(firstNumRE.FindString(line), stringDigits)
+		last := digitToStr(lastNumRE.FindString(lineReverse), stringDigitsReverse)
+
+		calibrationValue, err := strconv.Atoi(first + last)
+
+		if err != nil {
+			log.Fatalf("Error: %v", err)
+		}
+
+		sum += calibrationValue
+	}
+
+	fmt.Println("Result:", sum)
+}
+
+func reverseSlice(s []string) (result []string) {
+	result = make([]string, len(s))
+	for i, v := range s {
+		for _, c := range v {
+			result[i] = string(c) + result[i]
+		}
+	}
+	return
+}
+
+func digitToStr(s string, m []string) string {
+	parsedValue, err := strconv.Atoi(s)
+	if err == nil {
+		return strconv.Itoa(parsedValue)
+	}
+	for i, value := range m {
+		if value == s {
+			return strconv.Itoa(i)
+		}
+	}
+
+	panic("No digit found")
 }
